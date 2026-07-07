46987d3f88491b11d85db3564f4ee270e95ccd6f
Nick <ap14141818@gmail.com>
2026-07-08 04:19:21 +0800

    統一Asset函式定義的位置

--- trce_airflow_run/nick_dag_self_generation_transaction.py
+++ trce_airflow_run/nick_dag_self_generation_transaction.py
@@ -4,12 +4,13 @@ from airflow.sdk import Asset
 from airflow.operators.python import PythonOperator
 # 💡 從你的任務腳本中引用對應的 def
 from nick_gcs_to_bigquery_tasks import import_self_generation_transaction_to_bq
+from common_config import GCS_BUCKET
 
 PROJECT_ID = "tibametopics"
 DATASET_ID = "trec_data"
 BQ_DATASET = "gcstobq_airflowtest"
 
-self_raw_asset = Asset(f"bq://{PROJECT_ID}.{BQ_DATASET}.self_raw")
+self_gcs_asset = Asset(f"gcs://{GCS_BUCKET}/self_generation_raw_data/")
 
 asset_self_raw = Asset(uri=f"bigquery://{PROJECT_ID}/{DATASET_ID}/trec_self_generation_transaction_raw")
 
@@ -28,7 +29,7 @@ with DAG(
     dag_id='gcs_to_bq_self_generation_transaction',
     default_args=default_args,
     description='任務三：從 GCS 讀取自發自用交易資料並寫入 BigQuery (帶說明)',
-    schedule=[self_raw_asset],
+    schedule=[self_gcs_asset],
     catchup=False,
     tags=['gcs', 'bigquery', 'raw'],
 ) as dag:
